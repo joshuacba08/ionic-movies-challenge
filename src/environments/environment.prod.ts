2b9db5083d932278f59688ebc6b57d6fd13951a6
joroya.hms <47667368+joshuacba08@users.noreply.github.com>
2023-02-08 13:44:38 -0300

    improvement: delete console logs

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  endpointServer: 'http://localhost:3000/api/v1'
+  endpointServer: 'https://ionic-movie-challenge-server-production.up.railway.app/api/v1'
 };
